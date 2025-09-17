bf2b6cfb8ceac2301da8174acd3c2fd3e7570b37
takadan1 <taka84dan@gmail.com>
2025-09-17 13:30:35 +0900

    （修正4）

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,5 +1,28 @@
+# app/models/share.rb
+
 class Share < ApplicationRecord
-    has_one_attached :image
-    has_many :comments, dependent: :destroy
-      belongs_to :user    
+  # --- 既存のアソシエーション（関連付け） ---
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  # --- Active Storageの設定 ---
+  has_one_attached :image
+
+  # --- ここからがバリデーションの追加部分 ---
+
+  # `about`（テキスト）と `image`（画像）のどちらか一方は必須、というカスタムバリデーション
+  validate :at_least_one_field_present
+
+  private
+
+  # カスタムバリデーションの具体的な処理を定義するメソッド
+  def at_least_one_field_present
+    # `about` テキストが空（nilまたは空文字列）で、かつ、`image` も添付されていない場合にエラーとする
+    if about.blank? && !image.attached?
+      # エラーメッセージを `errors` オブジェクトに追加する
+      # `:base` を指定することで、特定のフィールドに関連しない、モデル全体のエラーとして扱われる
+      errors.add(:base, "画像とコメントのどちらか一方は入力してください。")
+    end
+  end
+
 end
